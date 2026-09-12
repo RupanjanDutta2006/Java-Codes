4ff066eea7ea6afd12252f4040435bf2ef1a4757
RupanjanDutta2006 <rupanjandutta06@gmail.com>
2026-09-12 23:45:40 +0530

    Add basic string input example
    Add a simple Java program that prompts for a string using Scanner and prints it back to the console. This introduces a beginner-friendly example for basic input handling in Java.

---
+++ Fundamental/Basics/Str,java.java
@@ -0,0 +1,13 @@
+//WAP to take string input & print the string
+import java.util.Scanner;
+class Str
+{
+    public static void main(String[] as)
+    {
+      String x;
+      Scanner ob =new Scanner(System.in);
+      System.out.print("Enter the String = ");
+      x=ob.nextLine();
+      System.out.print("Name = "+x);    
+    }     
+}
