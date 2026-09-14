ecee8597047e0ee02347f6dd937131a0515c0520
RupanjanDutta2006 <rupanjandutta06@gmail.com>
2026-09-14 10:02:58 +0530

    Add arithmetic exception example
    Introduces a Java example that reads two integers, attempts division, and catches ArithmeticException to handle division-by-zero safely. It also includes a generic exception fallback for unexpected errors.

---
+++ Fundamental/Try And Catch/arithmetic_exception.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class arithmetic_exception
+{
+    public static void main(String as[]) 
+    {
+        try
+        {
+        int a,b,c;
+        Scanner ob =new Scanner(System.in);
+        System.out.print("Enter the 1st No. = ");
+        a=ob.nextInt();
+        System.out.print("Enter the 2nd No. = ");
+        b=ob.nextInt();    
+        c=a/b;
+        System.out.print("Division = "+c);
+        }
+        catch(ArithmeticException e)
+        {
+          System.out.print("Division by 0 not possible "+e); 
+        }
+        catch(Exception e)
+        {
+          System.out.print("Problem!!! "+e);  
+        }
+    }
+}
